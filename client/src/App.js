e5c82296c2395165da7a0332766d3015df6e2637
naylorna <naylorna@miamioh.edu>
2020-07-02 11:14:13 -0400

    Added search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 
-function App() {
-	const [drinkIng, setDrinks] = useState([])
-	useEffect(() => {
-		fetch("http://localhost:9000/api/drinks", {
-			method: "GET",
-			headers: {
-				"Access-Control-Allow-Origin":true
+class App extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			drinks: [],
+			searchValue: '',
+			addInput: '',
+			response: 'Search again sorry'
+		}
+		this.handleOnChange = this.handleOnChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+	}
+	
+	// Sets search value
+	handleOnChange = event => {
+			this.setState({ searchValue: event.target.value });
+	};
+
+	handleSearch = event => {
+		//this.componentDidMount();
+		if(this.state.drinks === this.state.searchValue) {
+				this.setState({response : 'Yes we have it'})
+				this.setState({ addInput: this.state.drinks})
 			}
-			}).then(response => {
-				if (response.status === 200) return response.json();
-      				throw new Error("failed to connect");
-			}).then(responseJSON => {
-      				setDrinks(drinkdrinkIng => [...drinkIng, responseJSON.drinkIngredients]);
-			});}, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Lets Drink!</h1>
-	  {drinkIng}
-      </header>
-    </div>
-  );
+	};
+
+	// Reading in the api data
+	componentDidMount() {
+		fetch('http://localhost:9000/api/drinks', {
+			method: 'GET',
+			headers: {
+    				'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com',
+    				'x-rapidapi-key': '4e646a0679msh6adc5431da3ebd5p1321dejsna473e2d5f6ed',
+    				useQueryString: true
+			  }
+		})
+		.then(res => res.json())
+		.then(data => {this.setState({ drinks: data.drinkIngredients.strIngredient1})})
+	}
+	render() {
+		return (
+			<div className="App">
+			<header className="App-header">
+				<h1>Lets Drink</h1>
+			</header>
+			<div>
+				<input name="text" type="text" placeholder="Search Alcohol" 
+					onChange={event => this.handleOnChange(event)} value={this.state.searchValue} />
+				<button onClick={this.handleSearch}>Search</button>
+				<p>{this.state.response}</p>
+				<button>Add to Coctail</button>
+        		</div>
+			<div id="recipe">
+				<p>Coctail Recipe:</p>
+			</div>
+			</div>
+		)
+	}
 }
+
 
 export default App;
