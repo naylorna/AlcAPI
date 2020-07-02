d72f6135337522e925ed8a0c9614e229a48b632c
naylorna <naylorna@miamioh.edu>
2020-07-02 12:34:22 -0400

    Created add button

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,38 @@ import './App.css';
 
 
 
-class App extends React.Component {
+class App extends React.Component {i
 	constructor(props){
 		super(props)
 		this.state = {
 			drinks: [],
 			searchValue: '',
 			addInput: '',
-			response: 'Search again sorry'
+			response: '',
+			recipe: []
 		}
 		this.handleOnChange = this.handleOnChange.bind(this);
 		this.handleSearch = this.handleSearch.bind(this);
+		this.addDrink = this.handleSearch.bind(this);
 	}
 	
 	// Sets search value
 	handleOnChange = event => {
 			this.setState({ searchValue: event.target.value });
 	};
+	
+	// Add drink to recipe
+	AddDrink = event => {
+		this.setState({ addInput: this.state.drinks})
+	}
 
 	handleSearch = event => {
 		//this.componentDidMount();
 		if(this.state.drinks === this.state.searchValue) {
 				this.setState({response : 'Yes we have it'})
-				this.setState({ addInput: this.state.drinks})
-			}
+		} else {
+			this.setState({response: 'Sorry search again'})
+		}
 	};
 
 	// Reading in the api data
@@ -41,7 +49,7 @@ class App extends React.Component {
 			  }
 		})
 		.then(res => res.json())
-		.then(data => {this.setState({ drinks: data.drinkIngredients.strIngredient1})})
+		.then(data => {this.setState({ drinks: data.drinkIngredients[0].strIngredient1})})
 	}
 	render() {
 		return (
@@ -54,10 +62,11 @@ class App extends React.Component {
 					onChange={event => this.handleOnChange(event)} value={this.state.searchValue} />
 				<button onClick={this.handleSearch}>Search</button>
 				<p>{this.state.response}</p>
-				<button>Add to Coctail</button>
+				<button onClick={this.addDrink}>Add to Coctail</button>
         		</div>
 			<div id="recipe">
 				<p>Coctail Recipe:</p>
+				<p>{this.state.addInput}</p>
 			</div>
 			</div>
 		)
